5f70b7eed0d375a0d9f557bef991e233435477c3
lilliflores <lillifloresay15@gmail.com>
2022-03-01 15:43:33 -0600

    todo

---
+++ .vscode/app.py
@@ -0,0 +1,8 @@
+from distutils.log import debug
+from flask import Flask
+app= Flask(__name__)
+@app.route("/")
+def home():
+    return "hello world"
+if __name__=="__main__":
+    app.run(debug=True)
